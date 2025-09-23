73f6dfa1707f6e628caf3f9fb9df587c8ef65713
Catjiara <EDCat@yandex.ru>
2025-09-23 23:28:52 +0300

    Initial commit

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,19 @@
 package jm.task.core.jdbc.util;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class Util {
     // реализуйте настройку соеденения с БД
+    public static final String DB_URL = "jdbc:mysql://localhost:3306/task_1_1_3";
+    public static final String DB_USERNAME = "root";
+    public static final String DB_PASSWORD = "Kamch25!";
+
+    public static Connection getConnection() throws ClassNotFoundException, SQLException{
+        System.out.println("Connecting to the database");
+        return DriverManager.getConnection(DB_URL, DB_USERNAME, DB_PASSWORD);
+    }
 }
+
+
